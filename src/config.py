4271f41bf9c8ea41d9f1d73fa2ba4e8f6cec4ba7
Devansh3712 <devanshamity@gmail.com>
2022-01-29 14:30:30 +0530

    Test slash commands

--- src/config.py
+++ src/config.py
@@ -2,6 +2,8 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     token: str
+    password: str
+    mongodb_uri: str
 
     class Config:
         env_file = ".env"
--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
+from typing import List
 from datetime import datetime
 import discord
 from discord.ext import commands
+from discord_slash import (
+    SlashCommand,
+    SlashContext
+)
+from discord_slash.utils.manage_commands import (
+    create_choice,
+    create_option
+)
 from .config import settings
 
+token = settings.token
 client = commands.Bot(
     command_prefix = "$",
     activity = discord.Activity(
@@ -10,10 +20,20 @@ client = commands.Bot(
         name = "wordle"
     )
 )
-token = settings.token
+slash = SlashCommand(client, sync_commands = True)
+guilds: List[int] = []
 
 @client.event
 async def on_ready():
+    guilds = [guild for guild in client.guilds]
     print(f"{datetime.now()} - Durdle bot is working")
+
+@slash.slash(
+    name = "hello",
+    description = "Greet the user",
+    guild_ids = guilds
+)
+async def _hello(ctx: SlashContext):
+    await ctx.send(f"Hello, {ctx.author.mention}")
 
 client.run(token)
